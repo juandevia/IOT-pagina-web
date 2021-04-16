dc122120160fd27d06922678e191d74914529dbb
jei-cm <69779388+jei-cm@users.noreply.github.com>
2021-04-16 14:44:41 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,11 +1,12 @@
 <?php
-
+// Basado en https://www.tutorialrepublic.com/php-tutorial/php-mysql-login-system.php
+// Inicializa una nueva sesión o identifica la sesión actual
 session_start();
-
-// Se verifica si el usuario esta logeado
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    //  Si no está logeado, se redirecciona a la pagina de inicio de sesión
-    header("location: login.php");
+ 
+// Se valida si el usuario ya esta loggeado
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    // Si sí, se redirecciona a la pagina de bienvenida
+    header("location: welcome.php");
     exit;
 }
 
@@ -17,87 +18,107 @@ define('DB_NAME', '3387047_chefcito');
 
 // Se conecta con la base de datos, retorna un objeto contenedor con la informacion de la conexión
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
+ 
 // Si la conexión no es posible con la base de datos
 if($link === false){
     die("ERROR: No se pudo conectar con la base de datos " . mysqli_connect_error());
 }
 
-// Se definen las variables y se inicializan con valores vacios
-$usergender = "";
-$weight = "";
-$height = "";
-$usergender_err = "";
-$weight_err = "";
-$height_err = "";
-
+// Se define las variables y se inicializan con valores vacios
+$username = "";
+$password = "";
+$username_err = "";
+$password_err = "";
+$login_err = "";
+ 
 // Se pregunta si ya se envió el formulario en el script html
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-    // Una vez  se envía el formulario al script php, se valida si los datos del usuario están vacíos
-    if(empty(trim($_POST["usergender"]))){
-        $usergender_err = "Por favor ingrese su sexo.";
+ 
+    // Una vez se envía el formulario al script php, se valida si el nombre de usuario está vacío
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Por favor ingrese un Nombre de Usuario.";
     } else{
-        $usergender = trim($_POST["usergender"]); // variable $usergender toma valor 'usegender' dado en el metodo POST
-    }
-   
-    // Se valida si sí se ingresó una peso a partir de la entrada 'weight' dada en el metodo 'POST'
-    if(empty(trim($_POST["weight"]))){
-        $weight_err = "Por favor ingrese su peso.";     
-    } 
-     else{ // Se guarda el peso en la variable 'weight'
-        $weight = trim($_POST["weight"]);
+        $username = trim($_POST["username"]); // variable $username toma valor 'username' dado en el metodo POST
     }
     
-    // Se valida si sí se ingresó una altura a partir de la entrada 'height' dada en el metodo 'POST'
-    if(empty(trim($_POST["height"]))){
-        $height_err = "Por favor ingrese su altura.";     
+    // Se valida si la contraseña dada en el formulario esta vacia
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Por favor ingrese su contraseña.";
     } else{
-        $height = trim($_POST["height"]); // Se guarda la altura en la variable 'height'
+        $password = trim($_POST["password"]); // variable $password toma valor 'password' dado en el metodo POST
     }
     
-    // Si nunca se necesitaron de las variables auxilaires de error, es porque los datos están bien
-    if(empty($usergender_err) && empty($weight_err) && empty($height_err)){
-        
-        // Se prepara un estado de inserción de datos en la base de datos
-        $sql = "UPDATE Usuarios SET usergender = ?, weight = ?, height = ? WHERE id = ?";
-         
-        // Se prepara la ejecucion de el estado de inserción solicitado   
-        if($stmt = mysqli_prepare($link, $sql)){
-        
-            // Se unen los parametros $param_usergender, $param_weight, $param_height a las variables del statement "stmt"
-            mysqli_stmt_bind_param($stmt, "siii", $param_usergender, $param_weight, $param_height, $param_id);
-            
-            $param_id = $_SESSION["id"];  
-            $param_usergender = $usergender; // Se establece el $param_usergender de acuerdo a la variabel $usergender
-            $param_weight = $weight; //  el resultado se guarda en $param_weight
-            $param_height = $height; //  el resultado se guarda en $param_height
+    // Si los datos ingresados en el formulario no están vacios
+    if(empty($username_err) && empty($password_err)){
 
-            // Se intenta ejecutar el estado de inserción solicitado
+        // Prepare un estado de seleccion
+        $sql = "SELECT id, username, password FROM Usuarios WHERE username = ?";
+        
+        // Se prepara la ejecucion de el estado de selección solicitado
+        if($stmt = mysqli_prepare($link, $sql)){
+
+            // Si sí se puede realizar la ejecución de la lectura de datos solicitado:
+            // Se une el parametro $param_username a las variables del statement "stmt"
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            
+            // Se establecen los parametros
+            $param_username = $username;
+            
+            // Intento de ejecutar el estado seleccionado
             if(mysqli_stmt_execute($stmt)){
-                // Si la ejecución es correcta, entonces se escribieron los datos en la base de datos
-                header("location: welcome.php"); // Se redirecciona a la pagina de user
+
+                // Se guarda el resultado como tabla de estados en en la variable 'stmt'
+                mysqli_stmt_store_result($stmt);
+                
+                // Si la tabla de resultados tiene una sola fila, quiere decir que el nombre de usuario ya existe
+                if(mysqli_stmt_num_rows($stmt) == 1){   
+
+                    // Se vinculan los parámetros $id, $username, $hashed_password a los resultados en la tabla de estados "stmt"
+                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+
+                    // Se extraen los resultados del estado de selección preparado
+                    if(mysqli_stmt_fetch($stmt)){
+                        // Si sí se pudieron extraer correctamente:
+                        // Se verifica si la contraseña es correcta
+                        if(password_verify($password, $hashed_password)){
+                            // Si la contraseña es correcta, se inicia una nueva sesion
+                            session_start();
+                            // Se guardan los datos en las variables de la nueva sesión
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["username"] = $username;                            
+                            // Se redirecciona el usuario a la pagina de bienvenida
+                            header("location: welcome.php");
+                        } else{
+                            // La contraseña es incorrecta
+                            $login_err = "Usuario o Contraseña incorrectos.";
+                        }
+                    }
+                } else{
+                    // El Nombre de Usuario no existe
+                    $login_err = "Usuario invalido.";
+                }
             } else{
-                // No se pudo ejecutar el estado de inserción
-                echo "Ahhhh! Errores. Por favor intente mas tarde.";
+                echo "Ahhh! Errores. Por favor intente mas tarde.";
             }
 
-            // Se cierra la tabla de estados de la solicitud
+            // Se cierra el estado de seleccion
             mysqli_stmt_close($stmt);
         }
     }
     
-    // Se cierra la conexion con la base de datos
+    // Se cierra la conexion
     mysqli_close($link);
 }
 ?>
-
+ 
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registro de datos</title>
+    <title>Inicia Sesión</title>
     <link rel="stylesheet" href="css/estilos.css">
     <style>
         body{ font: 14px sans-serif; }
@@ -106,65 +127,61 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 </head>
 <body>
 
-    <!-- Encabezado de la pagina-->
-    <header>
+     <!-- Encabezado de la pagina-->
+     <header>
         <nav class="contenedor-flex-1">
 
             <a href="index.html">
                 <img src= "img/logo.png" alt="logo" width="250" height="100">
             </a>
-            <ul class="nav"><!--lista no ordenanda con los enlaces-->
+            <ul><!--lista no ordenanda con los enlaces-->
                 <li><a href="index.html">Inicio </a></li>
                 <li><a href="nosotrosl.html">Nosotros</a></li>
                 <li><a href="contacto.html">Proyecto</a></li>
-                <li><a href="welcome.php">Sesión</a>
-                    <ul>
-                        <li><a href="reset_password.php">Cambiar contraseña</a></li>
-                        <li><a href="data.php">Datos usuario</a></li>
-                        <li><a href="logout.php">Cerrar Sesión</a></li> 
-                    </ul>
-                </li>
+                <li><a href="login.php">Inicia Sesión</a></li>
+
             </ul>
         </nav>
     </header>
-
-    <!-- Clase para crear el formulario de registro de datos -->
-    <div class="wrapper">
-        <h2>Por favor llena tus datos personales:</h2>
-        <p></p>
+    
+    <!-- Clase para crear el formulario de inicio de sesión -->
 
         <?php 
         if(!empty($login_err)){
             echo '<div class="alert alert-danger">' . $login_err . '</div>';
         }        
         ?>
-    </div>
+        
+    
+
         <!-- Se crea el formulario para ser enviado a al codigo php -->
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+    
             <div class="form-group">
-                <label>Género</label>
-                <input type="text" name="usergender" class="form-control <?php echo (!empty($usergender_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $usergender; ?>">
-                <span class="invalid-feedback"><?php echo $usergender_err; ?></span>
-            </div>   
+                <h2>Login</h2>
+            </div>
+            <div class="form-group">
+                <label>Usuario</label>
+                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                <span class="invalid-feedback"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group">
+                <label>Contraseña</label>
+                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
+                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Iniciar Sesión">
+            </div>
             
-            <div class="form-group">
-                <label>Peso</label>
-                <input type="number" name="weight" class="form-control <?php echo (!empty($weight_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $weight; ?>">
-                <span class="invalid-feedback"><?php echo $weight_err; ?></span>
-            </div>
-
-            <div class="form-group">
-                <label>Altura</label>
-                <input type="number" name="height" class="form-control <?php echo (!empty($height_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $height; ?>">
-                <span class="invalid-feedback"><?php echo $height_err; ?></span>
-            </div>
-            
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Enviar">
-                <input type="reset" class="btn btn-secondary ml-2" value="Reset">
-            </div>
+            <section class="cuota">
+            <span>¿Aun no estás registrado?</span> 
+            <a href="registro.php">Registrate</a>
+            </section>
+        
         </form>
-
+        
+    
     
     <!--El pie de pagina -->
     <footer>
@@ -172,4 +189,5 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     </footer>
     
 </body>
+
 </html>
