86c47d817966ade307e970fa01e7eb760bee93c6
jei-cm <69779388+jei-cm@users.noreply.github.com>
2021-05-25 01:13:57 -0500

    Update new_pass.php

--- new_pass.php
+++ new_pass.php
@@ -1,6 +1,14 @@
 <?php
-// Basado en https://www.tutorialrepublic.com/php-tutorial/php-mysql-login-system.php
-
+// Inicializa una nueva sesión o identifica la sesión actual
+session_start();
+ 
+// Se verifica si el usuario esta logeado
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    //  Si no está logeado, se redirecciona a la pagina de inicio de sesión
+    header("location: login.php");
+    exit;
+}
+ 
 // Credenciales para conectar a la base de datos.
 define('DB_SERVER', 'fdb19.125mb.com');
 define('DB_USERNAME', '3387047_chefcito');
@@ -14,85 +22,65 @@ $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if($link === false){
     die("ERROR: No se pudo conectar con la base de datos " . mysqli_connect_error());
 }
-
-// Se define las variables y se inicializan con valores vacios
-$pass = "";
-$pass_err = "";
-$newpass = "";
-$newpass_err = "";
+ 
+// Se definen las variables y se inicializan con valores vacios
+$new_password = "";
+$confirm_password = "";
+$new_password_err = "";
+$confirm_password_err = "";
  
 // Se pregunta si ya se envió el formulario en el script html
 if($_SERVER["REQUEST_METHOD"] == "POST"){
  
-    // Una vez se envía el formulario al script php, se valida si el nombre de usuario está vacío
-    if(empty(trim($_POST["username"]))){
-        $username_err = "Por favor ingrese un Nombre de Usuario.";
+    // Una vez se envía el formulario al script php, se valida que la nueva contraseña no está vacío
+    if(empty(trim($_POST["new_password"]))){
+        $new_password_err = "Por favor ingrese una nueva contraseña.";     
+    } elseif(strlen(trim($_POST["new_password"])) < 6){ // Se valida que la nueva contraseña tenga al menos 6 caracteres
+        $new_password_err = "La contraseña debe tener al menos 6 caracteres.";
     } else{
-        $username = trim($_POST["username"]); // variable $username toma valor 'username' dado en el metodo POST
+        $new_password = trim($_POST["new_password"]); // variable $new_password toma valor 'new_password' dado en el metodo POST
     }
     
-    // Se valida si la contraseña dada en el formulario esta vacia
-    if(empty(trim($_POST["password"]))){
-        $password_err = "Por favor ingrese su contraseña.";
+    // Se valida la confirmacion de la nueva contraseña contraseña
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Por favor confirme la contraseña.";
     } else{
-        $password = trim($_POST["password"]); // variable $password toma valor 'password' dado en el metodo POST
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($new_password_err) && ($new_password != $confirm_password)){
+            $confirm_password_err = "Las contraseñas no coinciden.";
+        }
     }
-    
-    // Si los datos ingresados en el formulario no están vacios
-    if(empty($username_err) && empty($password_err)){
-
-        // Prepare un estado de seleccion
-        $sql = "SELECT id, username, password FROM Usuarios WHERE username = ?";
         
-        // Se prepara la ejecucion de el estado de selección solicitado
+    // Si los datos ingresados en el formulario están correctos
+    if(empty($new_password_err) && empty($confirm_password_err)){
+
+        // Se prepara un estado de actualizacion
+        $sql = "UPDATE Usuarios SET password = ? WHERE id = ?";
+        
+        // Se prepara la ejecucion de el estado de actualización solicitado
         if($stmt = mysqli_prepare($link, $sql)){
 
             // Si sí se puede realizar la ejecución de la lectura de datos solicitado:
-            // Se une el parametro $param_username a las variables del statement "stmt"
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            // Se une el parametro string $param_username, integer $param_id a las variables de la tabla de estados "stmt"
+            mysqli_stmt_bind_param($stmt, "si", $param_password, $param_id);
             
             // Se establecen los parametros
-            $param_username = $username;
+            $param_password = password_hash($new_password, PASSWORD_DEFAULT); // Se escribe la nueva contraseña en el parametro de la tabla de estados
+            $param_id = $_SESSION["id"];     // Se identifica el 'id' del usuario al que se le cambiará la contraseña
             
-            // Intento de ejecutar el estado seleccionado
+            // Intento de ejecutar el estado actual
             if(mysqli_stmt_execute($stmt)){
 
-                // Se guarda el resultado como tabla de estados en en la variable 'stmt'
-                mysqli_stmt_store_result($stmt);
-                
-                // Si la tabla de resultados tiene una sola fila, quiere decir que el nombre de usuario ya existe
-                if(mysqli_stmt_num_rows($stmt) == 1){   
+                // Si se logra ejecutar el estado de actualización solicitado, entonces la contraseña se cambió correctamente
+                session_destroy();          // Se destruye la sesion y se redirecciona a la pagina de login
+                header("location: login.php");
+                exit();
 
-                    // Se vinculan los parámetros $id, $username, $hashed_password a los resultados en la tabla de estados "stmt"
-                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
-
-                    // Se extraen los resultados del estado de selección preparado
-                    if(mysqli_stmt_fetch($stmt)){
-                        // Si sí se pudieron extraer correctamente:
-                        // Se verifica si la contraseña es correcta
-                        if(password_verify($password, $hashed_password)){
-                            // Si la contraseña es correcta, se inicia una nueva sesion
-                            session_start();
-                            // Se guardan los datos en las variables de la nueva sesión
-                            $_SESSION["loggedin"] = true;
-                            $_SESSION["id"] = $id;
-                            $_SESSION["username"] = $username;                            
-                            // Se redirecciona el usuario a la pagina de bienvenida
-                            header("location: welcome.php");
-                        } else{
-                            // La contraseña es incorrecta
-                            $login_err = "Usuario o Contraseña incorrectos.";
-                        }
-                    }
-                } else{
-                    // El Nombre de Usuario no existe
-                    $login_err = "Usuario invalido.";
-                }
             } else{
                 echo "Ahhh! Errores. Por favor intente mas tarde.";
             }
 
-            // Se cierra el estado de seleccion
+            // Se cierra el estado iniciado
             mysqli_stmt_close($stmt);
         }
     }
@@ -108,7 +96,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicia Sesión</title>
+    <title>Reiniciar Contraseña</title>
     <link rel="stylesheet" href="css/estilos.css">
     <style>
         body{ font: 14px sans-serif; }
@@ -117,68 +105,58 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 </head>
 <body>
 
-     <!-- Encabezado de la pagina-->
-     <header>
+    <!-- Encabezado de la pagina-->
+    <header>
         <nav class="contenedor-flex-1">
 
             <a href="index.html">
                 <img src= "img/logo.png" alt="logo" width="250" height="100">
             </a>
-            <ul><!--lista no ordenanda con los enlaces-->
+            <ul class="nav"><!--lista no ordenanda con los enlaces-->
                 <li><a href="index.html">Inicio </a></li>
                 <li><a href="nosotrosl.html">Nosotros</a></li>
                 <li><a href="contacto.html">Proyecto</a></li>
-                <li><a href="login.php">Inicia Sesión</a></li>
-
+                <li><a href="welcome.php">Sesión</a>
+                    <ul>
+                        <li><a href="reset_password.php">Cambiar contraseña</a></li>
+                        <li><a href="data.php">Datos usuario</a></li>
+                        <li><a href="logout.php">Cerrar Sesión</a></li> 
+                    </ul>
+                </li>
             </ul>
         </nav>
     </header>
-    
-    <!-- Clase para crear el formulario de inicio de sesión -->
 
-        <?php 
-        if(!empty($login_err)){
-            echo '<div class="alert alert-danger">' . $login_err . '</div>';
-        }        
-        ?>
-        
-    
-
-        <!-- Se crea el formulario para ser enviado a al codigo php -->
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-    
-            <div class="form-group">
-                <h2>Restauración de la contraseña</h2>
+    <!-- Clase para crear el formulario de reestablecimiento de contraseña -->
+    <div class= "form-group">
+            <div class="wrapper">
+                <div class="form-group">
+                    <h2>   </h2>
+                </div>
+                <!-- Se crea el formulario para ser enviado a al codigo php -->
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post"> 
+                    <div class="form-group">
+                        <label>Nueva Contraseña</label>
+                        <input type="password" name="new_password" class="form-control <?php echo (!empty($new_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $new_password; ?>">
+                        <span class="invalid-feedback"><?php echo $new_password_err; ?></span>
+                    </div>
+                    <div class="form-group">
+                        <label>Confirmar Contraseña</label>
+                        <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>">
+                        <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-primary" value="Enviar">
+                        <a class="btn btn-link ml-2" href="welcome.php">Cancelar</a>
+                    </div>
+                </form>
             </div>
-            <div class="form-group">
-                <label>Nueva contraseña</label>
-                <input type="text" name="username" class="form-control <?php echo (!empty($pass_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
-                <span class="invalid-feedback"><?php echo $pass_err; ?></span>
-            </div>    
-            <div class="form-group">
-                <label>Confirmar contraseña</label>
-                <input type="password" name="password" class="form-control <?php echo (!empty($newpass_err)) ? 'is-invalid' : ''; ?>">
-                <span class="invalid-feedback"><?php echo $newpass_err; ?></span>
-            </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Enviar">
-            </div>
+     </div>
             
-            <section class="cuota">
-            <span>¿Aun no estás registrado?</span> 
-            <a href="registro.php">Registrate</a>
-            </section>
-        
-        </form>
-        
-    
-    
-    <!--El pie de pagina -->
+        <!--El pie de pagina -->
     <footer>
         <div> CHEFCITO corporation &copy; Todos los derechos reservados</div>
     </footer>
     
 </body>
-
 </html>
-
