e3ebf0d3e1b3a75e9ef6dd4b00b04593bfdf6975
Juan Camilo Devia Bastos <juandevia03@gmail.com>
2021-05-25 12:12:04 -0500

    arreglo_datos

--- data.php
+++ data.php
@@ -143,19 +143,230 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="form-group">
                 <label>Género</label>
-                <input type="text" name="usergender" class="form-control <?php echo (!empty($usergender_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $usergender; ?>">
+                    <select name="usergender">
+                        <option value=""></option>
+                        <option value="Masculino">Masculino</option>
+                        <option value="Femenino">Femenino</option>
+                    </select>
+                <name="usergender" class="form-control <?php echo (!empty($usergender_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $usergender; ?>">
                 <span class="invalid-feedback"><?php echo $usergender_err; ?></span>
             </div>   
             
             <div class="form-group">
                 <label>Peso</label>
-                <input type="number" name="weight" class="form-control <?php echo (!empty($weight_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $weight; ?>">
+
+                <select name="weight">
+                        <option value=""></option>
+                        <option value="40">40Kg</option>
+                        <option value="41">41Kg</option>
+                        <option value="42">42Kg</option>
+                        <option value="43">43Kg</option>
+                        <option value="44">44Kg</option>
+                        <option value="45">45Kg</option>
+                        <option value="46">46Kg</option>
+                        <option value="47">47Kg</option>
+                        <option value="48">48Kg</option>
+                        <option value="49">49Kg</option>
+                        <option value="50">50Kg</option>
+
+                        <option value="51">51Kg</option>
+                        <option value="52">52Kg</option>
+                        <option value="53">53Kg</option>
+                        <option value="54">54Kg</option>
+                        <option value="55">55Kg</option>
+                        <option value="56">56Kg</option>
+                        <option value="57">57Kg</option>
+                        <option value="58">58Kg</option>
+                        <option value="59">59Kg</option>
+                        <option value="60">60Kg</option>
+
+                        <option value="61">61Kg</option>
+                        <option value="62">62Kg</option>
+                        <option value="63">63Kg</option>
+                        <option value="64">64Kg</option>
+                        <option value="65">65Kg</option>
+                        <option value="66">66Kg</option>
+                        <option value="67">67Kg</option>
+                        <option value="68">68Kg</option>
+                        <option value="69">69Kg</option>
+                        <option value="70">70Kg</option>
+
+                        
+                        <option value="71">71Kg</option>
+                        <option value="72">72Kg</option>
+                        <option value="73">73Kg</option>
+                        <option value="74">74Kg</option>
+                        <option value="75">75Kg</option>
+                        <option value="76">76Kg</option>
+                        <option value="77">77Kg</option>
+                        <option value="78">78Kg</option>
+                        <option value="79">79Kg</option>
+                        <option value="80">80Kg</option>
+
+                       
+                        <option value="81">81Kg</option>
+                        <option value="82">82Kg</option>
+                        <option value="83">83Kg</option>
+                        <option value="84">84Kg</option>
+                        <option value="85">85Kg</option>
+                        <option value="86">86Kg</option>
+                        <option value="87">87Kg</option>
+                        <option value="88">88Kg</option>
+                        <option value="89">89Kg</option>
+                        <option value="90">90Kg</option>
+
+                      
+                        <option value="91">91Kg</option>
+                        <option value="92">92Kg</option>
+                        <option value="93">93Kg</option>
+                        <option value="94">94Kg</option>
+                        <option value="95">95Kg</option>
+                        <option value="96">96Kg</option>
+                        <option value="97">97Kg</option>
+                        <option value="98">98Kg</option>
+                        <option value="99">99Kg</option>
+                        <option value="100">100Kg</option>
+
+                      
+                        <option value="101">101Kg</option>
+                        <option value="102">102Kg</option>
+                        <option value="103">103Kg</option>
+                        <option value="104">104Kg</option>
+                        <option value="105">105Kg</option>
+                        <option value="106">106Kg</option>
+                        <option value="107">107Kg</option>
+                        <option value="108">108Kg</option>
+                        <option value="109">109Kg</option>
+                        <option value="110">110Kg</option>
+
+                       
+                        <option value="111">111Kg</option>
+                        <option value="112">112Kg</option>
+                        <option value="113">113Kg</option>
+                        <option value="114">114Kg</option>
+                        <option value="115">115Kg</option>
+                        <option value="116">116Kg</option>
+                        <option value="117">117Kg</option>
+                        <option value="118">118Kg</option>
+                        <option value="119">119Kg</option>
+                        <option value="120">120Kg</option>
+
+            
+                        <option value="121">121Kg</option>
+                        <option value="122">122Kg</option>
+                        <option value="123">123Kg</option>
+                        <option value="124">124Kg</option>
+                        <option value="125">125Kg</option>
+                        <option value="126">126Kg</option>
+                        <option value="127">127Kg</option>
+                        <option value="128">128Kg</option>
+                        <option value="129">129Kg</option>
+                        <option value="130">130Kg</option>
+                    </select>
+
+
+                <name="weight" class="form-control <?php echo (!empty($weight_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $weight; ?>">
                 <span class="invalid-feedback"><?php echo $weight_err; ?></span>
             </div>
 
             <div class="form-group">
                 <label>Altura</label>
-                <input type="number" name="height" class="form-control <?php echo (!empty($height_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $height; ?>">
+
+                <select name="height">
+                        <option value=""></option>
+                        <option value="140">1.40m</option>
+                        <option value="141">1.41m</option>
+                        <option value="142">1.42m</option>
+                        <option value="143">1.43m</option>
+                        <option value="144">1.44m</option>
+                        <option value="145">1.45m</option>
+                        <option value="146">1.46m</option>
+                        <option value="147">1.47m</option>
+                        <option value="148">1.48m</option>
+                        <option value="149">1.49m</option>
+                        <option value="150">1.50m</option>
+
+                        <option value="151">1.51m</option>
+                        <option value="152">1.52m</option>
+                        <option value="153">1.53m</option>
+                        <option value="154">1.54m</option>
+                        <option value="155">1.55m</option>
+                        <option value="156">1.56m</option>
+                        <option value="157">1.57m</option>
+                        <option value="158">1.58m</option>
+                        <option value="159">1.59m</option>
+                        <option value="160">1.60m</option>
+
+                        <option value="161">1.61m</option>
+                        <option value="162">1.62m</option>
+                        <option value="163">1.63m</option>
+                        <option value="164">1.64m</option>
+                        <option value="165">1.65m</option>
+                        <option value="166">1.66m</option>
+                        <option value="167">1.67m</option>
+                        <option value="168">1.68m</option>
+                        <option value="169">1.69m</option>
+                        <option value="170">1.70m</option>
+
+                        <option value="171">1.71m</option>
+                        <option value="172">1.72m</option>
+                        <option value="173">1.73m</option>
+                        <option value="174">1.74m</option>
+                        <option value="175">1.75m</option>
+                        <option value="176">1.76m</option>
+                        <option value="177">1.77m</option>
+                        <option value="178">1.78m</option>
+                        <option value="179">1.79m</option>
+                        <option value="180">1.80m</option>
+
+                        <option value="181">1.81m</option>
+                        <option value="182">1.82m</option>
+                        <option value="183">1.83m</option>
+                        <option value="184">1.84m</option>
+                        <option value="185">1.85m</option>
+                        <option value="186">1.86m</option>
+                        <option value="187">1.87m</option>
+                        <option value="188">1.88m</option>
+                        <option value="189">1.89m</option>
+                        <option value="190">1.90m</option>
+
+                        <option value="191">1.91m</option>
+                        <option value="192">1.92m</option>
+                        <option value="193">1.93m</option>
+                        <option value="194">1.94m</option>
+                        <option value="195">1.95m</option>
+                        <option value="196">1.96m</option>
+                        <option value="197">1.97m</option>
+                        <option value="198">1.98m</option>
+                        <option value="199">1.99m</option>
+                        <option value="200">2.0m</option>
+
+                        <option value="201">2.01m</option>
+                        <option value="202">2.02m</option>
+                        <option value="203">2.03m</option>
+                        <option value="204">2.04m</option>
+                        <option value="205">2.05m</option>
+                        <option value="206">2.06m</option>
+                        <option value="207">2.07m</option>
+                        <option value="208">2.08m</option>
+                        <option value="209">2.09m</option>
+                        <option value="210">2.10m</option>
+
+                        <option value="211">2.11m</option>
+                        <option value="212">2.12m</option>
+                        <option value="213">2.13m</option>
+                        <option value="214">2.14m</option>
+                        <option value="215">2.15m</option>
+                        <option value="216">2.16m</option>
+                        <option value="217">2.17m</option>
+                        <option value="218">2.18m</option>
+                        <option value="219">2.19m</option>
+                        <option value="220">2.20m</option>
+
+                </select>
+
+
+                <name="height" class="form-control <?php echo (!empty($height_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $height; ?>">
                 <span class="invalid-feedback"><?php echo $height_err; ?></span>
             </div>
             
